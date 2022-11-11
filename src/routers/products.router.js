3b9c7bcd72c83b66c628f80abc67944abd4ff197
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2022-11-11 19:57:16 -0300

    feat: Add validateName middleware at post products router

--- src/routers/products.router.js
+++ src/routers/products.router.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { productsController } = require('../controllers/index');
+const validateNameField = require('../middlewares/validateNameField');
 
 const router = express.Router();
 
 router.get('/', productsController.findAllProducts);
 router.get('/:id', productsController.findProductById);
-router.post('/', productsController.createNewProduct);
+router.post('/', validateNameField, productsController.createNewProduct);
 
 module.exports = router;
