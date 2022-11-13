10ca01b5134a65bf1704fe608b2892ffcf30bcb9
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2022-11-13 20:59:36 -0300

    feat: Create test for products controller function findAllProducts sucess request

--- tests/unit/controllers/products.controller.test.js
+++ tests/unit/controllers/products.controller.test.js
@@ -7,6 +7,7 @@ chai.use(sinonChai);
 
 const { productsService } = require('../../../src/services');
 const { productsController } = require('../../../src/controllers');
+const controllerMock = require('./Mocks/products.controller.mock');
 
 describe('Test controller layer of products path', function () {
   afterEach(sinon.restore);
@@ -26,6 +27,23 @@ describe('Test controller layer of products path', function () {
     expect(res.status).to.have.been.calledWith(500);
     expect(res.json).to.have.been.calledWith({ message: 'Internal server error!' });
   });
+
+  it('should return status 200 and message with "Internal server error!"', async function () {
+    const res = {};
+    const req = {};
+
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+
+    sinon.stub(productsService, 'getAllProducts')
+      .resolves({ type: null, message: controllerMock.allProducts });
+
+    await productsController.findAllProducts(req, res);
+
+    expect(res.status).to.have.been.calledWith(200);
+    expect(res.json).to.have.been.calledWith(controllerMock.allProducts);
+  });
+
 
   
 });
