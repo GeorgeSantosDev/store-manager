1ae10eb5c5cd66fc7150fbd88aed2569b695ad2e
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2022-11-15 17:02:27 -0300

    feat: Create SQL query for insert new sales in database

---
+++ src/models/sales.model.js
@@ -0,0 +1,26 @@
+// const camelize = require('camelize');
+const snakeize = require('snakeize');
+const connection = require('./connection');
+const currentDate = require('../utils/currentDate');
+
+const insertSales = async (sale) => {
+  const columns = Object.keys(snakeize(sale));
+  const values = Object.values(sale);
+  const placeholders = Object.values(sale).map((_) => '?');
+
+  const newSale = await connection.execute(
+    `INSERT INTO StoreManager.sales_products (${columns}) VALUES (${placeholders})`,
+    [...values],
+  );
+
+   await connection.execute(
+    'INSERT INTO StoreManager.sales (date) VALUES (?)',
+     [currentDate()],
+  );
+
+  return newSale;
+};
+
+module.exports = {
+  insertSales,
+};
