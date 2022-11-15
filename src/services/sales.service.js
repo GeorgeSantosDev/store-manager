a73cc928543b43677dfca26bf10630a60a97b168
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2022-11-15 19:28:20 -0300

    feat: Add new validation into service function insertNewSale

--- src/services/sales.service.js
+++ src/services/sales.service.js
@@ -1,13 +1,18 @@
 const { salesModel } = require('../models');
+const validations = require('./validations/validateIfProductIdExist');
 
 const insertNewSale = async (sales) => {
+  const allIdsValid = await validations.productExist(sales);
+
+  if (!allIdsValid) return { type: 'PRODUCT_NOT_FOUND', message: 'Product not found' };
+
   const id = await salesModel.insertSaleId();
   const salesWithId = sales.map((sale) => ({ ...sale, saleId: id }));
 
   const promises = salesWithId.map((sale) => salesModel.insertSales(sale));
   const newSales = await Promise.all(promises);
 
-  if (newSales) return { type: null, message: newSales };
+  if (newSales) return { type: null, message: { id, itemsSold: sales } };
 
   return { type: 'SALE_NOT_ADDED', message: 'Sale not added' };
 };
