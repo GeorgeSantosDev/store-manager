fb45beef600c6976db7801a3c0e4fd498b193018
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2022-11-15 18:26:36 -0300

    feat: Create middleware for validate quantity field

--- src/middlewares/validateSalesInformation.js
+++ src/middlewares/validateSalesInformation.js
@@ -9,6 +9,15 @@ const validateIdField = (req, res, next) => {
   return res.status(errorMap.INVALID_FIELD).json({ message: '"productId" is required' });
 };
 
+const validateQuantityField = (req, res, next) => {
+  const sales = req.body;
+  const everyoneHasQuantity = sales.every((sale) => 'quantity' in sale);
+
+  if (everyoneHasQuantity) return next();
+
+  return res.status(errorMap.INVALID_FIELD).json({ message: '"quantity" is required' });
+};
+
 module.exports = {
   validateIdField,
 };
