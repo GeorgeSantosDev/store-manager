88386ebae8563c3d06c7e1103f58a444274dab8b
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2022-11-15 22:17:25 -0300

    feat: Create products model function update

--- src/models/products.model.js
+++ src/models/products.model.js
@@ -37,9 +37,21 @@ const findProductByName = async (name) => {
   return result;
 };
 
+const update = async (id, name) => {
+  const [result] = await connection.execute(
+    `UPDATE StoreManager.products
+    SET name = ?
+    WHERE id = ?`,
+    [name, id],
+  );
+
+  return result;
+};
+
 module.exports = {
   findAll,
   findById,
   insert,
   findProductByName,
+  update,
 };
