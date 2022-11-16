873ca96f33d3b7e8c2030eb26011ec08cf99523c
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2022-11-16 17:50:59 -0300

    feat: Create sales controller function deleteSale

--- src/controllers/sales.controller.js
+++ src/controllers/sales.controller.js
@@ -27,8 +27,18 @@ const getSaleById = async (req, res) => {
   res.status(200).json(message);
 };
 
+const deleteSale = async (req, res) => {
+  const { id } = req.params;
+  const { type, message } = await salesService.deleteSaleById(id);
+
+  if (type) return res.status(errorMap.mapError(type)).json({ message });
+
+  res.status(204).json();
+};
+
 module.exports = {
   addNewSales,
   getSales,
   getSaleById,
+  deleteSale,
 };
