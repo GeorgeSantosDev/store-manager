6288f1ef96f7e35d0fbacff1b9ad147dc6502bb5
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2022-11-16 15:11:42 -0300

    test: Create new mock value

--- tests/unit/services/Mocks/sales.service.mock.js
+++ tests/unit/services/Mocks/sales.service.mock.js
@@ -5,6 +5,28 @@ const newSale = [
   }
 ];
 
+const allSales = [
+  {
+    saleId: 2,
+    productId: 3,
+    quantity: 15,
+    date: '2022-11-16T17:06:10.000Z'
+  },
+  {
+    saleId: 3,
+    productId: 2,
+    quantity: 1,
+    date: '2022-11-16T17:29:39.000Z'
+  },
+  {
+    saleId: 4,
+    productId: 2,
+    quantity: 1,
+    date: '2022-11-16T17:30:22.000Z'
+  }
+]
+
 module.exports = {
   newSale,
+  allSales,
 };
