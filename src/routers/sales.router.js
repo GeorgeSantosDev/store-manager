f27942ffe674fdd06bd35c0bb908e4eb0fe439fa
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2022-11-16 18:30:15 -0300

    feat: Create new put sales router

--- src/routers/sales.router.js
+++ src/routers/sales.router.js
@@ -12,5 +12,10 @@ router.post('/',
   validateSalesInformation.validateQuantityFieldValue,
   salesController.addNewSales);
 router.delete('/:id', salesController.deleteSale);
+router.put('/:id',
+  validateSalesInformation.validateIdField,
+  validateSalesInformation.validateQuantityField,
+  validateSalesInformation.validateQuantityFieldValue,
+  salesController.updateSale);
 
 module.exports = router;
