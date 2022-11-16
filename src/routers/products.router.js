3b6884cf1598d1d4642496b0375f1e9c7437a18c
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2022-11-16 20:18:07 -0300

    feat: Create new get  products  router

--- src/routers/products.router.js
+++ src/routers/products.router.js
@@ -5,6 +5,7 @@ const validateNameField = require('../middlewares/validateNameField');
 const router = express.Router();
 
 router.get('/', productsController.findAllProducts);
+router.get('/search', productsController.findProductByName);
 router.get('/:id', productsController.findProductById);
 router.post('/', validateNameField, productsController.createNewProduct);
 router.put('/:id', validateNameField, productsController.updateProduct);
